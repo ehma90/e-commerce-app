f5276c1d3547bbb8e05c38f8e8ac31b4275a0e4e
Emmanuel Essien <ehmaessien@gmail.com>
2023-02-24 10:00:02 +0100

    create ProductItem component, Update global style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import Layout from "@/components/Layout";
+import ProductItem from "@/components/ProductItem";
+import data from "utilities/data";
 
 export default function Home({title}) {
   return (
     <>
       <Layout title="Home Page">
-        Layout
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => (
+            <ProductItem key={product.slug} product={product}/>
+          ))}
+        </div>
       </Layout>
     </>
   )
