ec7ce1a0bd43de879045f468f664f02ca481ab9f
Emmanuel Essien <ehmaessien@gmail.com>
2023-02-27 15:10:25 +0100

    Update loginScreen component

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ This is an e-commerce web application that does the baisc functionalities that r
 - global layout component
 - Slug page for individual product.
 - Add to cart depending on the availiable stock.
-- install js cookie to prevent refreshing item cart when page reloads.
-- 
+- Install js cookie to prevent refreshing item cart when page reloads.
+- Install React-hook-form to validate users.
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,37 +1,53 @@
+import { useForm } from "react-hook-form";
 import Layout from "@/components/Layout";
 import Link from "next/link";
 import React from "react";
 
 export default function LoginScreen() {
+    const {handleSubmit, register, formState: {errors}} = useForm()
+
+    const submitHandler = ({email, password}) => {
+        console.log(email, password);
+    }
   return (
     <Layout title="Login">
-      <form className="mx-auto max-w-screen-md">
+      <form className="mx-auto max-w-screen-md" onSubmit={handleSubmit(submitHandler)}>
         <h1 className="mb-4 text-xl">Login</h1>
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
             type="email"
+            {...register("email", {required: "Please enter Email", pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: "Please Enter valid email",
+            }})}
             className="w-full px-2"
             placeholder="Enter email"
             id="email"
             autoFocus
           />
+          {errors.email && (<div className="text-red-500">{errors.email.message}</div>)}
         </div>
         <div className="mb-4">
           <label htmlFor="password">Password</label>
           <input
-            type="password "
+            type="password"
+            {...register("password", {required: "Please enter password", minLength: {
+                value: 6,
+                message: "Password is more than 5 characters"
+            }})}
             className="w-full px-2"
             placeholder="Enter password"
             id="password"
             autoFocus
           />
+          {errors.password && (<div className="text-red-500">{errors.password.message}</div>)}
         </div>
         <div className="mb-4">
             <button className="primary-button"> Login </button>
         </div>
         <div className="mb-4">
-            Don&apos;t have an account? &nbsp
+            Don&apos;t have an account? &nbsp;
             <Link href="/register">Register</Link>
         </div>
       </form>
