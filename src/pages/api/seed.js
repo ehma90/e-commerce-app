053f855d9a5c138b8edf99240d5b3dfc90e523b1
Emmanuel Essien <ehmaessien@gmail.com>
2023-03-25 16:06:16 +0100

    Update product model

--- src/pages/api/seed.js
+++ src/pages/api/seed.js
@@ -1,11 +1,14 @@
 const { default: data } = require("@/utilities/data")
 const { default: db } = require("@/utilities/db")
-const { default: User } = require("models/user")
+const { default: User } = require("/models/User")
+const { default: Product } = require("/models/Product")
 
 const handler = async (req, res) => {
     await db.connect()
     await User.deleteMany()
     await User.insertMany(data.users)
+    await Product.deleteMany()
+    await Product.insertMany(data.products)
     await db.disconnect()
     res.send({message: "seeded Successfully"})
 }
